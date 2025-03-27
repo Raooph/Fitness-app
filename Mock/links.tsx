da5b37c7b870a7a32b0df42e8ccbff9beeaa0b66
(Raooph) <(raufquliyev829@gmail.com)>
2025-03-28 03:29:03 +0400

    mock data created

---
+++ Mock/links.tsx
@@ -0,0 +1,7 @@
+import { ILinks } from "@/Interface/links";
+
+export const links:ILinks[]=[
+    { id: 0, title: "Staff", to: "/staff" },
+    { id: 1, title: "Users", to: "/users" },
+    { id: 2, title: "Cards", to: "/cards" },
+]
